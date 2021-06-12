9f757f794126356907b1d3cf34215494e5cb239c
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2021-06-12 12:36:54 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
-from  article import views
+from article import views
 
 router = routers.DefaultRouter()
 router.register(r'article', views.ArticleView, 'article')
