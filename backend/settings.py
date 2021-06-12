a605e5356152fff8fde156e71e1d1423de4c4a60
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2021-06-12 13:21:00 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 CORS_ORIGIN_WHITELIST = [
-     'http://adityaray.netlify.app'
+     'https://adityaray.netlify.app/'
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
